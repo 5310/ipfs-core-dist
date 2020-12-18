e66c20bca712fe493b3f06466470ce248c4f3078
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2020-12-18 12:58:46 +0530

    Clean up the source

--- index.ts
+++ index.ts
@@ -1,16 +1,3 @@
-// This is a shim to make Js-IPFS work within TypeScript
-//
-// The NPM packages `ipfs` and `ipfs-core` both have type annotations
-// using docstrings, but because they're still not ESM capable
-// and only ships Node code by default Snowpack et al try to 
-// import it into the browser which fails.
-//
-// The solution is to import the browser distribution from within
-// and then override it with the type from the root module.
-//
-// Thankfully TypeSciprt can do that. But it'd still be better if
-// we could cast an imported module directly in the import statement.
-
 // @ts-ignore
 import IPFS_ from 'ipfs-core/dist/index.min.js'
 import type IPFS__ from 'ipfs-core'
